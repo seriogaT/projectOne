10f2ec13f0f5fe2266f95665304801620eb7a434
seriogaT <serghei.tovarnitchi@gmail.com>
2017-09-25 11:44:18 +0300

    Adding a project to github

---
+++ src/test/java/runTest.java
@@ -0,0 +1,9 @@
+import org.junit.runner.RunWith;
+import cucumber.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@Cucumber.Options( features = "D:\\intellij-workspace\\src\\test\\java\\Annotation\\features" )
+
+public class runTest {
+
+}
